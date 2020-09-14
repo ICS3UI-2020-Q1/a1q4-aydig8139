902c023aa5689d5b0f6d66bf2d899a3f88132eba
aydig8139 <aydig8139@wrdsb.ca>
2020-09-14 18:19:20 +0000

    Completed question so the coe provides the slope for any given two coordinates

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Calculates the slope when given two coordinates
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,39 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates a Scanner used for input
+    Scanner input = new Scanner(System.in);
+
+    // Ask user for first point
+    System.out.println("Please enter in the x coordinate of the first point");
+
+    // Declare and initialize a variable for the user to put in a number
+    int x1 = input.nextInt();
+
+    // Ask user for second point
+    System.out.println("Please enter in the y coordinate of the first point");
+
+    // Declare and initialize a variable for the user to put in a number
+    int y1 = input.nextInt();
+
+    // Ask user for third point
+    System.out.println("Please enter in the x coordinate of the second point");
+
+    // Declare and initialize a variable for the user to put in a number
+    int x2 = input.nextInt();
+
+    // Ask user for third point
+    System.out.println("Please enter in the y coordinate of the second point");
+
+    // Declare and initialize a variable for the user to put in a number
+    int y2 = input.nextInt();
+
+    // calculate slope
+    double slope = (y2 - y1) / (x2 - x1);
+
+    // Tell user the slope
+
+    System.out.println("The slope of your line would be " + slope);
     
   }
 }
